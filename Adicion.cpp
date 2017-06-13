0d8f6717b352421f444cccdf13446ede8409a119
juanmanuelfrei <jfrei@net2one.com.ar>
2017-06-13 19:38:09 -0300

    Add files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,23 @@
+/*
+	"Adicion"
+	"Juan Manuel Bottarelli Frei"
+	"20170403"
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int a, b;
+	
+	cout << "El siguiente programa mostrara la suma de dos valores ingresados por el usuario" << endl << endl;
+	
+	cout << "Ingrese el primer valor" << endl;
+	cin >> a;
+	cout << "Ingrese el segundo valor" << endl;
+	cin >> b;
+
+	cout << "La suma de los valores ingresados es:" << a+b << endl;
+	
+}
